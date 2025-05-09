20853991ba59e7ce6f1a476d9d40faf8c795a843
Jacopo <jacopo.dilauro04@gmail.com>
2025-05-10 00:12:42 +0200

    Update run.sh

--- NonsenseGenerator_LARJ/run.sh
+++ NonsenseGenerator_LARJ/run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-printf "\r\033[K- Progetto NosenceGenerator -\n"
+printf "\r\033[K- NosenceGenerator Project -\n"
 # Percorso assoluto alla chiave JSON
-printf "🗝️​ Imposto la chiave..."
+printf "🗝️​ Setting the key..."
 export GOOGLE_APPLICATION_CREDENTIALS="$(pwd)/google-key.json"
 
 # Compila il progetto
-printf "\r\033[K🔧 Compilazione in corso..."
+printf "\r\033[K🔧 Compiling in progress..."
 mvn -q compile
 
 # Esegui la classe Java principale
-printf "\r\033[K🚀 Avvio del programma..."
+printf "\r\033[K🚀 Starting program..."
 mvn -q exec:java -Dexec.mainClass="com.nonsense.LanguageExample"
